9cfa5d02f2c6d49bdb4966b5a50eedb92526d4ee
jxd6575 <jxd6575@g.rit.edu>
2025-03-09 23:33:35 -0400

    B: Added xming and got a black screen working

--- main.py
+++ main.py
@@ -3,9 +3,23 @@
 # throughout this file
 import pygame
 
-def main():
-    print("Starting Asteroids!")
+from constants import *
 
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        #print(pygame.get_init())
+        screen.fill("black")
+        pygame.display.flip()
+    
+    
 
 if __name__ == "__main__":
         main()
